a1297c100d2b34da5162fc2dcdaae84c84ca8b8e
Zachary Shannon <Zachary.Shannon@microfocus.com>
2023-06-26 16:31:41 +0100

    Use programs not entry points

--- dataset_builder/humaneval_to_cbl.py
+++ dataset_builder/humaneval_to_cbl.py
@@ -7,13 +7,21 @@ DOCSTRING_LINESTART_RE = re.compile("""\n(\s+)""")
 
 class Translator:
     
-    stop=["goback."]
+    stop=["\ngoback."]
 
     def __init__(self):
         pass
     
-    def cbl_preamble(self) -> str:
-        return "procedure division.\n"
+    def cbl_preamble(self, name) -> str:
+        return "\n".join([
+                "identification division.",
+                "program-id "+name,
+                "working-storage section.",
+                "\n"
+        ])
+    
+    def gen_ws(self) -> str:
+        pass
 
     def translate_prompt(self, name: str, args: List[ast.arg], _returns, description: str) -> str:
         # Set up globals
@@ -23,16 +31,19 @@ class Translator:
         arg_list = ""
         for arg in args:
             arg_list = arg_list + arg.arg + " "
-        return f"{self.cbl_preamble()}{cbl_description}entry \"{name}\" using by value {arg_list}.\n"
+        return f"{self.cbl_preamble(name)}\nprocedure division using by value {arg_list}."
     
     def test_suite_prefix_lines(self, entry_point) -> List[str]:
         """
         Code for start of test suite.
         """
-
         return ["goback.",
-                "\n"
-                "entry \"test_prog\"."]
+                "\n",
+                "identification division",
+                "program-id. test_prog.",
+                "working-storage section.",
+                "procedure division."
+                ]
     
     def test_suite_suffix_lines(self) -> List[str]:
         """
@@ -44,6 +55,21 @@ class Translator:
         All tests are assertions that compare deep equality between left and right.
         """
         return f"{left}returning abc.\nif abc = {right}\n    return true\nelse\n    return false\nend-if"
+    
+    def gen_call(self, func: str, args: List[str]) -> str:
+        """Translate a function call `func(args)`
+        A function call f(x, y, z) translates to f(x, y, z)
+        """
+        arg_list = ""
+        for arg in args:
+            arg_list = arg_list + arg + " "
+
+        if func == "candidate":
+            func_name = self.entry_point
+        else:
+            func_name = func
+
+        return f"call \"{func_name}\" using by value {arg_list}"
     
     # Below are todo. Produces typescript.
 
@@ -72,18 +98,3 @@ class Translator:
 
     def gen_dict(self, keys: List[str], values: List[str]) -> str:
         return "{" + ", ".join(f"{k}: {v}" for k, v in zip(keys, values)) + "}"
-
-    def gen_call(self, func: str, args: List[str]) -> str:
-        """Translate a function call `func(args)`
-        A function call f(x, y, z) translates to f(x, y, z)
-        """
-        arg_list = ""
-        for arg in args:
-            arg_list = arg_list + arg + " "
-
-        if func == "candidate":
-            func_name = self.entry_point
-        else:
-            func_name = func
-
-        return f"call \"{func_name}\" using by value {arg_list}"
