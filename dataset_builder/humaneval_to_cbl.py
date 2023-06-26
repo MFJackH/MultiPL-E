4ba3502128e0d979d87755184956078dc71c02a6
Jack Holt <jack.holt@microfocus.com>
2023-06-26 11:11:26 +0100

    Adding blank translator module for COBOL code

---
+++ dataset_builder/humaneval_to_cbl.py
@@ -0,0 +1,9 @@
+# This script translates problems from the OpenAI HumanEval dataset into COBOL.
+import re
+import ast
+from typing import List, Dict, Tuple
+
+class Translator:
+
+    def __init__(self):
+        pass
