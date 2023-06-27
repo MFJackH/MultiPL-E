8e4884dbc386450b70da8ed95d494e240ff90517
Alex Hutson <alex.hutson@microfocus.com>
2023-06-27 11:20:12 +0100

    check_translation_compile script for testing if prompt and tests are compilable

---
+++ dataset_builder/check_translation_compile.py
@@ -0,0 +1,89 @@
+import argparse
+from generic_translator import list_originals, translate_prompt_and_tests
+import os
+import subprocess
+
+TESTERS = {
+    "cpp": { "translator": "humaneval_to_cpp", "build": "g++", "extension": "cpp" },
+    "cbl": { "translator": "humaneval_to_cbl","build": "cbllink", "extension": "cbl" }
+}
+test_translation_path = "translated_prompts_and_tests"
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "--lang", type=str, help="Language to translate to"
+    )
+    args.add_argument(
+        "--doctests",
+        type=str,
+        default="keep",
+        help="What to do with doctests: keep, remove, or transform",
+    )
+    args.add_argument(
+        "--prompt-terminology",
+        type=str,
+        default="verbatim",
+        help="How to translate terminology in prompts: verbatim or reworded"
+    )
+    args.add_argument(
+        "--originals",
+        type=str,
+        help="Originals to use for translation",
+        default="../datasets/originals"
+    )
+    parsed_args = args.parse_args()
+
+    # Delete exist test translations
+    if os.path.exists(test_translation_path):
+        for file in os.listdir(test_translation_path):
+            joined_file = os.path.join(test_translation_path, file)
+            os.remove(joined_file)
+        os.rmdir(test_translation_path)
+
+    os.makedirs(test_translation_path)
+
+    translator_use = __import__(TESTERS[parsed_args.lang]["translator"]).Translator()
+
+    # Loop through all problems in chosen set
+    originals = list_originals(parsed_args.originals)
+    compiler_fails = 0
+    skip_fails = 0
+    for original in originals.values():
+        result = translate_prompt_and_tests(original, translator_use, parsed_args.doctests, parsed_args.prompt_terminology)
+
+        if result is None:
+            print(f"Translation fail for: {original}")
+            skip_fails+= 1
+            continue
+
+        (prompt, tests) = result
+
+        # Create compilable file of correct form
+        file_name = os.path.basename(original)
+        file_name_no_extension = os.path.splitext(file_name)[0]
+        extension = "." + TESTERS[parsed_args.lang]["extension"]
+        file_path = os.path.join(test_translation_path, file_name_no_extension)
+        file_path_extension = file_path + extension
+
+        with open(file_path_extension, "w") as file:
+            print(f"Writing prompt translation to {file_path_extension}")
+            content = prompt + tests
+            file.write(content)
+        
+        # Add compilation process here
+        if(parsed_args.lang == "cbl"):
+            result = subprocess.run([TESTERS[parsed_args.lang]["build"], file_path_extension], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        elif(parsed_args.lang == "cpp"):
+            result = subprocess.run([TESTERS[parsed_args.lang]["build"], "-o", file_path_extension, "-std=c++17"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            raise Exception("No implementation for this language")
+
+        if (result.returncode):
+            compiler_fails += 1
+
+    print(f"Translation fails {skip_fails} / {len(originals)}")
+    print(f"Compiler fails {compiler_fails} / {len(originals)}")
+
+if __name__ == "__main__":
+    main()
