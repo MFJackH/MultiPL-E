fc4171db336c2ec77bd4b3d9e38673a12bbd407e
Alex Hutson <alex.hutson@microfocus.com>
2023-06-29 20:20:20 +0100

    Modify extraction script and fix the JSON read for the problems list

--- extract_src.py
+++ extract_src.py
@@ -1,5 +1,5 @@
 """
-This Script takes a .json.gz file and extracts the prompt and completed source into a file that can be compiled.
+This Script takes a directory filled with with .json.gz completions, and creates a fully formed program.
 """
 
 import argparse
@@ -10,44 +10,42 @@ import os
 def get_args():
     args = argparse.ArgumentParser()
     args.add_argument(
-        "--file",
+        "--completions-dir",
         type=str,
         required=True,
-        help="file to extract from. Must be a json.gz file"
-    )
-    args.add_argument(
-        "--output-dir",
-        type=str,
-        required=False
+        help="Directory to extract from. Must contain json.gz files to turn into a program."
     )
     return args.parse_args()
 
 def main():
     args = get_args()
-    filename = args.file
-    with gzip.open(filename, 'r') as fin:
-        data = json.loads(fin.read().decode('utf-8'))
+    completions_dir = args.completions_dir
+    for file in os.listdir(completions_dir):
+        filename = os.path.join(completions_dir, file)
+        with gzip.open(filename, 'r') as fin:
+            data = json.loads(fin.read().decode('utf-8'))
 
-    prompt = data["prompt"]
-    completions = data["completions"]
-    test = data["tests"]
+        prompt = data["prompt"]
+        completions = data["completions"]
+        test = data["tests"]
 
-    outfile_base = ""
-    if args.output_dir:
-        outfile_base = args.output_dir
-    filename = os.path.basename(filename)
-    first_ext_gone = os.path.splitext(filename)[0]
-    filename = os.path.splitext(first_ext_gone)[0]
-    print(filename)
-    joined_path = os.path.join(outfile_base, filename)
+        first_ext_gone = os.path.splitext(file)[0]
+        base_name = os.path.splitext(first_ext_gone)[0]
+        outfile_base = base_name + "-completions"
+        outfile_base = os.path.join(completions_dir, outfile_base)
+        os.mkdir(outfile_base)
+        joined_path = os.path.join(outfile_base, base_name)
 
-    i = 0
-    for completion in completions:
-        outfile = joined_path + str(i) + ".cbl"
-        with open(outfile, 'w') as of:
-            of.write(prompt + completion + test)
-        print(f"File: {outfile} written to disk")
-        i +=1
+        i = 0
+        for completion in completions:
+            outfile = joined_path + str(i) + ".cbl"
+            with open(outfile, 'w') as of:
+                try:
+                    of.write(prompt + completion + test)
+                except:
+                    continue
+            print(f"File: {outfile} written to disk")
+            i +=1
 
 
 if __name__ == "__main__":
